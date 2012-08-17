6729054a81a4e30490553554e15dfde19541e446
Max De Marzi <maxdemarzi@hotmail.com>
2012-08-17 08:18:49 -0500

    make sure a user exists before trying to use it

--- lib/neosocial/jobs/import_facebook_profile.rb
+++ lib/neosocial/jobs/import_facebook_profile.rb
@@ -4,31 +4,32 @@ module Job
 
     def perform(uid)
       user = User.find_by_uid(uid)
-      likes = user.client.get_connections("me", "likes")
+      if user
+        likes = user.client.get_connections("me", "likes")
 
-      if likes
-        # Import likes
-        commands = []
-        likes.each do |thing|
-          commands << [:create_unique_node, "thing_index", "uid", thing["id"], {"uid" => thing["id"], "name" => thing["name"] }]
+        if likes
+          # Import likes
+          commands = []
+          likes.each do |thing|
+            commands << [:create_unique_node, "thing_index", "uid", thing["id"], {"uid" => thing["id"], "name" => thing["name"] }]
+          end
+          batch_result = $neo_server.batch *commands
+
+          # Connect the user to these things
+          commands = []
+          batch_result.each do |b|
+            commands << [:create_unique_relationship, "likes_index", "user_thing",  "#{uid}-#{b["body"]["data"]["uid"]}", "likes", user.neo_id, b["body"]["self"].split("/").last]
+          end
+          $neo_server.batch *commands
         end
-        batch_result = $neo_server.batch *commands
 
-        # Connect the user to these things
-        commands = []
-        batch_result.each do |b|
-          commands << [:create_unique_relationship, "likes_index", "user_thing",  "#{uid}-#{b["body"]["data"]["uid"]}", "likes", user.neo_id, b["body"]["self"].split("/").last]
+        # Import Friends
+        friends = user.client.get_connections("me", "friends")
+        friends.each do |friend|
+          Sidekiq::Client.enqueue(Job::ImportFriends, uid, friend["id"])
+          Job::ImportMutualFriends.perform_at(120, uid, friend["id"])
         end
-        $neo_server.batch *commands
-      end
-
-      # Import Friends
-      friends = user.client.get_connections("me", "friends")
-      friends.each do |friend|
-        Sidekiq::Client.enqueue(Job::ImportFriends, uid, friend["id"])
-        Job::ImportMutualFriends.perform_at(120, uid, friend["id"])
       end
     end
-
   end
 end
--- lib/neosocial/jobs/import_friends.rb
+++ lib/neosocial/jobs/import_friends.rb
@@ -4,34 +4,35 @@ module Job
 
     def perform(uid, person_id)
       user = User.find_by_uid(uid)
-      person = user.client.get_object(person_id)
-      friend = User.create_from_facebook(person)
+      if user
+        person = user.client.get_object(person_id)
+        friend = User.create_from_facebook(person)
 
-      # Make them friends
-      commands = []
-      commands << [:create_unique_relationship, "friends_index", "ids",  "#{uid}-#{person_id}", "friends", user.neo_id, friend.neo_id]
-      commands << [:create_unique_relationship, "friends_index", "ids",  "#{person_id}-#{uid}", "friends", friend.neo_id, user.neo_id]
-      batch_result = $neo_server.batch *commands
-
-      # Import friend likes
-      likes = user.client.get_connections(person_id, "likes")
-
-      if likes
-        # Import things
+        # Make them friends
         commands = []
-        likes.each do |thing|
-          commands << [:create_unique_node, "thing_index", "uid", thing["id"], {"uid" => thing["id"], "name" => thing["name"] }]
-        end
+        commands << [:create_unique_relationship, "friends_index", "ids",  "#{uid}-#{person_id}", "friends", user.neo_id, friend.neo_id]
+        commands << [:create_unique_relationship, "friends_index", "ids",  "#{person_id}-#{uid}", "friends", friend.neo_id, user.neo_id]
         batch_result = $neo_server.batch *commands
 
-        # Connect the user to these things
-        commands = []
-        batch_result.each do |b|
-          commands << [:create_unique_relationship, "likes_index", "user_thing",  "#{person_id}-#{b["body"]["data"]["uid"]}", "likes", friend.neo_id, b["body"]["self"].split("/").last]
-        end
-        $neo_server.batch *commands
-      end
+        # Import friend likes
+        likes = user.client.get_connections(person_id, "likes")
 
+        if likes
+          # Import things
+          commands = []
+          likes.each do |thing|
+            commands << [:create_unique_node, "thing_index", "uid", thing["id"], {"uid" => thing["id"], "name" => thing["name"] }]
+          end
+          batch_result = $neo_server.batch *commands
+
+          # Connect the user to these things
+          commands = []
+          batch_result.each do |b|
+            commands << [:create_unique_relationship, "likes_index", "user_thing",  "#{person_id}-#{b["body"]["data"]["uid"]}", "likes", friend.neo_id, b["body"]["self"].split("/").last]
+          end
+          $neo_server.batch *commands
+        end
+      end
     end
 
   end
